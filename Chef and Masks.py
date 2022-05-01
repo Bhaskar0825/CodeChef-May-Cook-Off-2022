1939698ae394ae65c378d5085978fd5fea163c71
Bhaskar Gupta <82353317+Bhaskar0825@users.noreply.github.com>
2022-05-01 20:14:22 +0530

    Create Chef and Masks.py

---
+++ Chef and Masks.py
@@ -0,0 +1,7 @@
+#If you are Seeing this Please Subscribe to the Channel: https://bit.ly/3F4Jxnd 
+for i in range(int(input())):
+    X,Y = map(int,input().split())
+    if (X*100)>=(Y*10):
+        print("Cloth")
+    else:
+        print("Disposable")
